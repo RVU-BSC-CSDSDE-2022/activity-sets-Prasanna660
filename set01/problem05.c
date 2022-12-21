19c15b5fc424b9b30b19dc16a5b7a7f70e3c6662
Prasanna 	 Gurdev <115770359+Prasanna660@users.noreply.github.com>
2022-12-21 08:36:30 +0000

    sol ved

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 int input();
 int compare(int a, int b, int c);
-void output(int a, int b, int c);
+void output(int a, int b, int c, int largest);
+
+int input()
+{
+  int n;
+  printf("Enter a number\n");
+  scanf("%d",&n);
+  return n;
+}
 
